553c493e840be602cae42d3748524e788ab67060
Breno213 <brenoleiteessi@gmail.com>
2022-05-22 20:31:30 -0300

    commit

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,8 +1,15 @@
 <?php
 
-  $hostname = "";
-  $database = "";
-  $user = "";
-  $passwords = "";
+  $hostname = "sql102.epizy.com";
+  $database = "epiz_31473897_sysrifa";
+  $user = "epiz_31473897";
+  $password = "9sghFBiKiA2";
 
-?>
+  try{
+    $pdo = new PDO("mysql:host=" .$hostname.";dbname="$database .$user .$password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexão com o Banco de Dados ".$database.", foi bem sucedida!";
+  } catch (PDOException $e) {
+    echo "Erro." .$e->getMessage();
+
+  }
